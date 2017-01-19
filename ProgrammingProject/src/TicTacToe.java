51d6fc29e243b201efe400ef4254c460694de55a
Laduona <gracefo1009@gmail.com>
2017-01-19 11:04:14 +0100

    Tictactoe

--- ProgrammingProject/src/TicTacToe.java
+++ ProgrammingProject/src/TicTacToe.java
@@ -8,31 +8,39 @@ import java.io.*;
  * @version $Revision: 1.4 $
  */
 public class TicTacToe {
+	private static class UncloseableInputStream extends FilterInputStream {
 
-	
-	public static Player determinePlayer(String arg, Mark mark) { 
-    	String argu = arg.toUpperCase();
-    	if (argu.equals("-N")) {
-    		return new ComputerPlayer(mark, new NaiveStrategy());
-    	}else if (argu.equals("-S")){
-    		return new ComputerPlayer(mark, new SmartStrategy()); 
-    	}else {
-    		return new HumanPlayer(arg, mark); 
-    	}
+        /**
+         * Creates a wrapper around {@link System.in}.
+         */
+        UncloseableInputStream() {
+            this(System.in);
+        }
+
+        /**
+         * Creates a wrapper around an arbitrary {@link InputStream}.
+         * @param stream The stream to wrap.
+         */
+        UncloseableInputStream(InputStream stream) {
+            super(stream);
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void close() throws IOException {
+            // Don't do anything
+        }
     }
 	
     public static void main(String[] args) {
-    	if (args.length ==2){
-    	    Player p1 = determinePlayer(args[0], Mark.XX); 
-        	Player p2 = determinePlayer(args[1], Mark.OO); 
-        	Game game = new Game(p1, p2); 
-        	game.start();
-    	}else{
-    		System.out.println("need more arguments");
-    	}
-    	
+    	System.setIn(new UncloseableInputStream());
+    	Player p1 = new HumanPlayer(args[0].toString(), Mark.OO);
+    	Player p2 = new HumanPlayer(args[1].toString(), Mark.XX);
+    	Game game = new Game(p1,p2);
+    	game.start();
     }
-    
     
     
 }
