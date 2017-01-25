1c32442841eef2339dfdbdbacaa2cb92cc206374
Laduona <gracefo1009@gmail.com>
2017-01-25 16:31:19 +0100

    player test unfinish

--- ProgrammingProject/src/Tests/PlayerTest.java
+++ ProgrammingProject/src/Tests/PlayerTest.java
@@ -1,14 +1,24 @@
 package Tests;
 
 import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
+import connect4.*;
 
 public class PlayerTest {
-
+	private Mark markRed;
+	private Mark markYel;
+	private Board board;
+	private HumanPlayer Hplayer;
+	private ComputerPlayer smartComputer;
+	private Strategy smart;
+	private Strategy naive;
+	
 	@Before
 	public void setUp() throws Exception {
+		board = new Board();
+		Hplayer = new HumanPlayer("human", markRed);
+		smartComputer = new ComputerPlayer(markYel, smart);
 	}
 
 	@Test
